afbd37476c5a73d6ecf194d7d9707b94aa6a85a4
Dinesh5544 <gembali.dinesh.s86@kalvium.community>
2025-04-23 09:39:37 +0530

    done

---
+++ index.js
@@ -0,0 +1,11 @@
+index.js
+const { encrypt, decrypt } = require('./script');
+
+const payload = { username: "kalvian123" };
+
+// Encrypt the payload
+const token = encrypt(payload);
+console.log("Encrypted Token:", token);
+
+// Decrypt the token
+const decoded = decrypt(token);
